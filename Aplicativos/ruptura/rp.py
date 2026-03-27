d136eecdca14036d50637931c2b7d936d4209f53
aludoq <aludoq@gmail.com>
2026-03-27 13:50:13 -0300

    Add files via upload

--- Aplicativos/ruptura/rp.py
+++ Aplicativos/ruptura/rp.py
@@ -12,7 +12,11 @@ from pathlib import Path
 
 # O rpcompra.csv não é mais utilizado. O Pipeline agora se alimenta do query.parquet
 base_dir = Path(__file__).parent
+<<<<<<< HEAD
+arquivo_entrada = Path('c:/Users/aludoq/Downloads/Equipes_Agentes/Aplicativos/import_querys/query.parquet')
+=======
 arquivo_entrada = Path('c:/Users/Alessandro.soares.BAKLIZI/Downloads/Equipes_Agentes/Aplicativos/import_querys/query.parquet')
+>>>>>>> c0d55e2759e64d63ae0ea39839df6f48aa226883
 
 import sys
 import subprocess
@@ -31,3 +35,15 @@ try:
     print("Dashboard gerado e atualizado.")
 except Exception as e:
     print(f"Ocorreu um erro ao encadear o dashboard: {e}")
+<<<<<<< HEAD
+
+# Executar a dashboard detalhada
+print("Executando a geração do Dashboard Detalhado...")
+try:
+    script_detalhado = base_dir / "dashboard_detalhado.py"
+    subprocess.run([sys.executable, str(script_detalhado)], check=True)
+    print("Dashboard detalhado gerado e atualizado.")
+except Exception as e:
+    print(f"Ocorreu um erro ao encadear o dashboard detalhado: {e}")
+=======
+>>>>>>> c0d55e2759e64d63ae0ea39839df6f48aa226883
