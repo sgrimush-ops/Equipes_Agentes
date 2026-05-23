7d3f3ee7c34122dbe3cd32fef0c455ea4babd302
Alessandro <sgrimush@gmail.com>
2026-05-23 10:55:59 -0300

    Atualização automática - 23/05/2026 10:55

--- Aplicativos/consumo/parquet_to_csv_confere.py
+++ Aplicativos/consumo/parquet_to_csv_confere.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 try:
     df = pd.read_parquet(r'C:\Users\usr\Downloads\Equipes_Agentes\Aplicativos\consumo\consumo.parquet')
-    df.to_csv('consumo_confere.csv', index=False, sep=';')
+    df.to_csv(r'C:\Users\usr\Downloads\Equipes_Agentes\Aplicativos\consumo\consumo_confere.csv', index=False, sep=';')
     print('Arquivo consumo_confere.csv gerado com sucesso!')
 except Exception as e:
     print(f'Erro ao processar: {e}')
