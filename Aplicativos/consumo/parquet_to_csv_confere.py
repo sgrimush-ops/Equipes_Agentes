1d9d1b482515e19e47cacf5b11753eb3303cdfd2
Alessandro <sgrimush@gmail.com>
2026-09-16 08:34:15 -0300

    Limpeza geral do ecossistema: remocao de arquivos temporarios, logs, venvs redundantes e rascunhos obsoletos

--- Aplicativos/consumo/parquet_to_csv_confere.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-try:
-    df = pd.read_parquet(r'C:\Users\usr\Downloads\Equipes_Agentes\Aplicativos\consumo\consumo.parquet')
-    df.to_csv(r'C:\Users\usr\Downloads\Equipes_Agentes\Aplicativos\consumo\consumo_confere.csv', index=False, sep=';')
-    print('Arquivo consumo_confere.csv gerado com sucesso!')
-except Exception as e:
-    print(f'Erro ao processar: {e}')
--- Aplicativos/gerenciamento_sql/scratch/ver_ge_pessoa.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('dicionario_consinco.db')
-c = conn.cursor()
-
-c.execute("SELECT NOME_COLUNA FROM colunas WHERE NOME_TABELA = 'GE_PESSOA' AND NOME_COLUNA IN ('SEQPESSOA', 'NOMERAZAO', 'FANTASIA', 'APELIDO')")
-print(c.fetchall())
-
-conn.close()
